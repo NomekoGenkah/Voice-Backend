92abbc2500161b6da7ce680e47e697417fafe930
Pedro Rojas <166954170+NomekoGenkah@users.noreply.github.com>
2024-11-10 19:55:48 -0300

    Update AuthService.java

--- src/main/java/com/back/demo/AuthService.java
+++ src/main/java/com/back/demo/AuthService.java
@@ -1,3 +1,36 @@
+package com.back.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import javax.crypto.spec.SecretKeySpec;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class AuthService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	//   ta listo CAMBIAR. ESTA FUNCION DEBE DEVOLVER TRUE SI LA MATRIZ DE USUARIO COINCIDE. SOLO DEBE HACER PULL DE LA DE USUARIO Y HACER LA MATRIZ DEL AUDIO SELECCIONADO
+    public boolean authenticateWithAudio(MultipartFile audioFile, String username) {
+		if (audioFile != null && !audioFile.isEmpty()) {
+			try {
+				Optional<User> userOptional = userRepository.findByUsername(username);
+				User user = userOptional.get();
+
+				System.out.println(audioFile.getOriginalFilename());
+				byte[] audioBytes = audioFile.getBytes();
+				//Hay que cambiar bytes per frame
+				double[] audioDouble = Matrix.convertToDoubleArray(audioBytes, 1);
+				double[] fft = FFT.Fourier(audioDouble);
+
+				double[][] matrix = Matrix.generarMatrix(fft, 5, 300, 1500, 16000.0f);
+				double[][] matrixUser = user.getMatrix();
 
 				if(Matrix.compareMatrix(matrix, matrixUser, 0.08)){
 					return true;
@@ -65,6 +98,9 @@
 			double[][] matrixKey = getUserKeyMatrix(username);
 			SecretKeySpec keySpec = Cifrar.generateAESKey(matrixKey);
 
+			//int[][] matrixInt = Matrix.convertMatrixToInt(matrixKey);
+			//byte[] matrixBytes = Cifrar.encryptWithMatrix(fileBytes, matrixKey);
+
 			byte[] encryptedData = Cifrar.encryptWithAES(fileBytes, keySpec);
 
 			return encryptedData;
@@ -80,7 +116,10 @@
 			double[][] matrixKey = getUserKeyMatrix(username);
 			SecretKeySpec keySpec = Cifrar.generateAESKey(matrixKey);
 
+			//int[][] matrixInt = Matrix.convertMatrixToInt(matrixKey);
+
 			byte[] decryptedData = Cifrar.decryptWithAES(fileBytes, keySpec);
+			//byte[] matrixBytes = Cifrar.decryptWithMatrix(decryptedData, matrixKey);
 
 			return decryptedData;
 		} catch (Exception e) {
@@ -89,6 +128,4 @@
 			// TODO: handle exception
 		}
 	}
-
-
 }
